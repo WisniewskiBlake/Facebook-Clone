035bfd1e93cbd74d70269d0e329f2dc1d0450756
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-01-28 03:04:31 -0500

    added notification model

--- app/src/main/java/com/example/facebookapp/activity/activity/model/NotificationModel.java
+++ app/src/main/java/com/example/facebookapp/activity/activity/model/NotificationModel.java
@@ -1,4 +1,108 @@
 package com.example.facebookapp.activity.activity.model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class NotificationModel {
+
+    @SerializedName("nid")
+    @Expose
+    private String nid;
+    @SerializedName("notificationTo")
+    @Expose
+    private String notificationTo;
+    @SerializedName("notificationFrom")
+    @Expose
+    private String notificationFrom;
+    @SerializedName("type")
+    @Expose
+    private String type;
+    @SerializedName("notificationTime")
+    @Expose
+    private String notificationTime;
+    @SerializedName("postId")
+    @Expose
+    private String postId;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("profileUrl")
+    @Expose
+    private String profileUrl;
+    @SerializedName("post")
+    @Expose
+    private Object post;
+
+    public String getNid() {
+        return nid;
+    }
+
+    public void setNid(String nid) {
+        this.nid = nid;
+    }
+
+    public String getNotificationTo() {
+        return notificationTo;
+    }
+
+    public void setNotificationTo(String notificationTo) {
+        this.notificationTo = notificationTo;
+    }
+
+    public String getNotificationFrom() {
+        return notificationFrom;
+    }
+
+    public void setNotificationFrom(String notificationFrom) {
+        this.notificationFrom = notificationFrom;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getNotificationTime() {
+        return notificationTime;
+    }
+
+    public void setNotificationTime(String notificationTime) {
+        this.notificationTime = notificationTime;
+    }
+
+    public String getPostId() {
+        return postId;
+    }
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProfileUrl() {
+        return profileUrl;
+    }
+
+    public void setProfileUrl(String profileUrl) {
+        this.profileUrl = profileUrl;
+    }
+
+    public Object getPost() {
+        return post;
+    }
+
+    public void setPost(Object post) {
+        this.post = post;
+    }
+
 }
